bf7e9f12db5c49a2fbe9abcfa8116c44529c8698
Kyle <44553820+KyleRokuKyu@users.noreply.github.com>
2023-03-14 21:53:35 -0400

    Base Spawning Going
    Players are assigned spawn points and can place their bases
    Extended the planet a bit

--- Assets/Scripts/BaseStuff/BaseSpawner.cs
+++ Assets/Scripts/BaseStuff/BaseSpawner.cs
@@ -1,17 +1,16 @@
 using Fusion;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseSpawner : NetworkTransform {
 	
 	public GameObject planet;
-	public LineRenderer line;
 
+	private Vector3 origin;
 	private bool placed;
 
 	private void Start()
 	{
+		origin = transform.position;
 		planet = GameObject.FindWithTag("Terrain");
 	}
 
@@ -23,20 +22,23 @@ public class BaseSpawner : NetworkTransform {
 		{
 			RaycastHit hit;
 			Vector3 ray = Input.mousePosition;
-			ray.z = 10;
+			ray.y = 1000;
+			ray.z = Camera.main.transform.position.z * 1.5f;
 			ray = Camera.main.ScreenToWorldPoint(ray);
 			ray.z = -1;
-			line.SetPosition(0, ray);
 
-			if (Physics.Linecast(ray, planet.transform.position, out hit))
+			print(Vector3.Angle(ray.normalized, origin.normalized));
+			if (Vector3.Angle(ray.normalized, origin.normalized) < 22)
 			{
-				transform.position = hit.point - new Vector3(0, 0, hit.point.z);
-				transform.LookAt(transform.position * 2f);
-				line.SetPosition(0, hit.point - new Vector3(0, 0, hit.point.z));
-
-				if (Input.GetButton("Fire1"))
+				if (Physics.Linecast(ray, planet.transform.position, out hit))
 				{
-					placed = true;
+					transform.position = hit.point - new Vector3(0, 0, hit.point.z);
+					transform.LookAt(transform.position * 2f);
+
+					if (Input.GetButton("Fire1"))
+					{
+						placed = true;
+					}
 				}
 			}
 		}
